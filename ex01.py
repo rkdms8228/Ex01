136ece1ec820cad132ec45883063a866af563302
가은 <rkdms8228@naver.com>
2022-07-15 10:48:15 +0900

    ex01

---
+++ ex01.py
@@ -0,0 +1,18 @@
+import keyword
+
+print("안녕")
+
+age = 13
+grant = 3
+
+print(age + grant)
+
+# 변수명
+my_name = "김가은"
+myname = "김가은"
+_myname = "김가은"
+num1 = 1
+
+# 파이썬 에약어 확인
+print(keyword.kwlist)
+print(len(keyword.kwlist))
